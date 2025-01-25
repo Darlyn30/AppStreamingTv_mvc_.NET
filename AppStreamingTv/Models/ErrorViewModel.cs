f97b16489f9ed0eee756feeba05c8429d00e3f58
darlyn <darlynolivop@gmail.com>
2025-01-25 16:57:24 -0400

    first injections files of project

---
+++ AppStreamingTv/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppStreamingTv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
